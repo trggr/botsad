b6125a9d4912efaa583022c8c2cdb8fa02e19b34
Tim Ashchepkov <tashepkov@gmail.com>
2021-02-26 14:03:03 -0500

    Reworked into function

--- src/botsad/core.clj
+++ src/botsad/core.clj
@@ -11,46 +11,6 @@
 (defn elim [s match replacement]
   (map #(str/replace % match replacement) s))
 
-(def id 112)
-
-(def p1 (slurp (str "site/" 112 ".html")))
-
-; On the page, the information is between <article> and </article>
-(def p2 (subs p1 (index-of p1 "<article>")
-                 (index-of p1 "</article>")))
-
-; each article has about 10 sections
-(def p3 (split p2 #"<div class="))
-
-; first section is useless
-(def p4 (rest p3))
-
-(def p5 (-> p4 
-            (elim #"\s+" " ")
-            (elim #"\"meta-heading\"> <h3 class=\"closed\">" "")
-            (elim #"\"meta-heading first\"> <h3 class=\"closed\">" "")
-            (elim #"\"meta-heading last\"> <h3 class=\"closed\">"  "")
-            (elim #"\"breadcrumb\">BGCI > Garden</div> <h1>"       "")
-            (elim #"</h1>"       "")
-            (elim #"<h3>"        "")
-            (elim #"<b>"         "")
-            (elim #"</b>"        "")
-            (elim #"</h3>"       "")
-            (elim #"</div>"      "")
-            (elim #"<div>"       "")
-            (elim #"<br />"      ";")
-            (elim #"</li> </ul>" ";")
-            (elim #"</li> <li>"  ";")
-            (elim #"<ul> <li>"   ";")
-            (elim #"\"edit-actions\">(.*)alt=\"Edit this page\" /></a>" "")
-            (elim #"<div id=\"tabs\">" "")
-            (elim #"<div id=\"tabs\">" "")
-            (elim #"<a href=\"#tabs-1\">Garden Information</a>; <div id=\"tabs-1\"> <p>" "")
-))
-
-(def p6 (let [[name info & others] p5
-              m (reduce merge (sorted-map) (map-indexed tomap others))]
-           (assoc m "0-ID" id "1-Name" name "2-Info" info)))
 
 ;(def p7 (join \newline 
 ;           (for [[k v] p6]
@@ -58,8 +18,6 @@
 ;
 ;(println p7)
 
-
-(def xls [(keys p6) (values p6)])
 
 ;; Create a spreadsheet and save it
 ;(let [wb (create-workbook "Price List"
@@ -72,12 +30,57 @@
 ;                                                     :font {:bold true}}))
 ;  (save-workbook! "spreadsheet2.xlsx" wb))
 
-(let [w      (create-workbook "Gardens" xls)
-      s      (select-sheet "Gardens" w)
-      header (first (row-seq s))]
-  (set-row-style! header (create-cell-style! w {:background :yellow, :font {:bold false}}))
-  (save-workbook! "spreadsheet3.xlsx" w))
+(defn export-xls [xls file]
+    (let [w      (create-workbook "Gardens" xls)
+          s      (select-sheet "Gardens" w)
+          header (first (row-seq s))]
+      (set-row-style! header (create-cell-style! w {:background :gray, :font {:bold false}}))
+      (save-workbook! file w)))
 
-(defn main [ & args]
-  (println "herro"))
+(def ids (map read-string (split (slurp "db/ids.txt") #"\n")))
+
+(defn parse-all [ids]
+    (for [id ids]
+       (let [p1 (slurp (str "site/" id ".html"))
+             p2 (subs p1 (index-of p1 "<article>") (index-of p1 "</article>"))
+             p3 (split p2 #"<div class=")   ; each article has about 10 sections
+             p4 (rest p3)                   ; the first one is useless
+             p5 (-> p4 
+                  (elim #"\s+" " ")
+                  (elim #"\"meta-heading\"> <h3 class=\"closed\">" "")
+                  (elim #"\"meta-heading first\"> <h3 class=\"closed\">" "")
+                  (elim #"\"meta-heading last\"> <h3 class=\"closed\">"  "")
+                  (elim #"\"breadcrumb\">BGCI > Garden</div> <h1>"       "")
+                  (elim #"</h1>"       "")
+                  (elim #"<h3>"        "")
+                  (elim #"<b>"         "")
+                  (elim #"</b>"        "")
+                  (elim #"</h3>"       "")
+                  (elim #"</div>"      "")
+                  (elim #"<div>"       "")
+                  (elim #"<br />"      ";")
+                  (elim #"</li> </ul>" ";")
+                  (elim #"</li> <li>"  ";")
+                  (elim #"<ul> <li>"   ";")
+                  (elim #"\"edit-actions\">(.*)alt=\"Edit this page\" /></a>" "")
+                  (elim #"<div id=\"tabs\">" "")
+                  (elim #"<div id=\"tabs\">" "")
+                  (elim #"<a href=\"#tabs-1\">Garden Information</a>; <div id=\"tabs-1\"> <p>" ""))
+             [name info & others] p5
+             m  (reduce merge (sorted-map) (map-indexed tomap others))]
+                (assoc m "0-ID" id "1-Name" name "2-Info" info))))
+
+; (parse-all ids)
+(set (flatten (map keys db)))
+
+;                (println (count db)
+;                         (keys (first db)
+;                         (vals (first db))))
+
+(defn main [& args] 1)
+
+
+
+
+
 
