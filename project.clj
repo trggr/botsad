79b3b82364505b39de54d05cc2039f86b734ca4d
Tim Ashchepkov <tashepkov@gmail.com>
2024-06-20 12:25:09 -0400

    Parsing works

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject botsad "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Botanic garden web scraping"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [dk.ative/docjure "1.14.0"]]
-  :repl-options {:init-ns botsad.core}
-  :main botsad.core/main)
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [dk.ative/docjure "1.14.0"]
+                 [reaver "0.1.3"]]
+  :main ^:skip-aot botsad.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+
--- src/botsad/core.clj
+++ src/botsad/core.clj
@@ -1,128 +1,78 @@
 (ns botsad.core
-  (:use [clojure.string :as str])
-  (:use [dk.ative.docjure.spreadsheet]))
+  (:require [reaver                       :as reaver]
+            [clojure.string               :as str]
+            [dk.ative.docjure.spreadsheet :as xls]))
 
-(defn tomap [idx item]
-   (->> (split item #";")
-        (map #(split % #":"))
-        (filter #(= (count %) 2))
-        (reduce (fn [acc [k v]] (assoc acc (str (+ 2 idx) "-" (trim k)) (trim v))) {})))
+(defn get-key-and-value
+  [s]
+  (when-let [pos (str/last-index-of s ":")]
+    [(str/trim (subs s 0 pos))
+     (str/trim (subs s (inc pos)))]))
 
-(defn elim [s match replacement]
-  (map #(str/replace % match replacement) s))
+(defn extract-meta-heading-fields
+  [parsed]
+  (->> (reaver/extract parsed [] ".meta-heading > ul > li" reaver/edn)
+       (map #(get % :content))
+       (map #(remove map? %))
+       flatten
+       (map str/trim)
+       (remove #(zero? (count %)))
+       (map get-key-and-value)
+       (remove nil?)))
 
+(defn get-contact-fields
+  [s]
+  (let [[_all _phone phone _fax fax _url url _email email]
+        (re-find #"(Telephone:)(.*)(Fax:)(.*)(URL:)(.*)(Primary Email:)(.*)" s)]
+    [["Phone" (str/trim phone)]
+     ["Fax" (str/trim fax)]
+     ["URL" (str/trim url)]
+     ["Email" (str/trim email)]]))
 
-(defn export-xls-old [xls file]
-    (let [w      (create-workbook "Gardens" xls)
-          s      (select-sheet "Gardens" w)
-          header (first (row-seq s))]
-      (set-row-style! header (create-cell-style! w {:background :gray, :font {:bold false}}))
-      (save-workbook! file w)))
+(defn get-familiar-field
+  [s]
+  (cond (or (str/starts-with? s "Institution Code:")
+            (str/starts-with? s "BGCI Member:")
+            (str/starts-with? s "Main Address:")) [(get-key-and-value s)]
+        (str/starts-with? s "Telephone:") (get-contact-fields s)
+        :else [["Mission" s]]))
 
-(defn export-xls [xls file]
-    (let [w      (create-workbook "Gardens" xls)
-          s      (select-sheet "Gardens" w)]
-      (save-workbook! file w)))
+(defn extract-familiar-fields
+  [parsed]
+  (->> (reaver/select parsed "article div#tabs-1 p")
+       (map reaver/text)
+       (mapcat get-familiar-field)))
 
-(def ids (map read-string (split (slurp "db/ids.txt") #"\n")))
+(defn extract-name-field
+  [parsed]
+  [["Name" (reaver/text (reaver/select parsed "article h1"))]])
 
-(defn parse-info [art]
-    (let [p1 (subs art (index-of art "<div id=\"tabs-1\">") (index-of art "<div class=\"meta-heading first\">"))
-          p2 (-> p1
-                 (replace #"<a.*?>" "")
-                 (replace #"<div.*?>" "")
-                 (replace #"<b>" "")
-                 (replace #"</b>" "")
-                 (replace #"</a>" "")
-                 (replace #"</p>" ";")
-                 (replace #"<p>"  "")
-                 (replace #"<p>" "")
-                 (replace #"<div>" "")
-                 (replace #"<h2>" "")
-                 (replace #"</h2>" "")
-                 (replace #"<br />" ";")
-                 (elim #"\s+" " "))
-           p3  (apply str p2)]
-      p3))
+(defn export-xls [file xls]
+  (let [w  (xls/create-workbook "Gardens" xls)
+        _  (xls/select-sheet "Gardens" w)]
+    (xls/save-workbook! file w)))
 
+(defn work
+  [urls]
+  (doseq [url urls]
+    (println "Processing" url)
+    (let [[_all _slash basename _ext] (re-find #"(/|\\)?(\d+)(.html|htm)" url)
+          parsed (-> url slurp (str/replace #"</?[bi]>" "")
+                     reaver/parse)]
 
-(defn parse-all [ids]
-    (for [id ids]
-       (let [p1  (slurp (str "site/" id ".html"))
-             p2  (subs p1 (index-of p1 "<article>") (index-of p1 "</article>"))
-             info (parse-info p2)
-             p3  (split p2 #"<div class=")   ; each article has about 10 sections
-             p4  (rest p3)                   ; the first one is useless
-             p5  (-> p4 
-                   (elim #"\s+" " ")
-                   (elim #"\"meta-heading\"> <h3 class=\"closed\">" "")
-                   (elim #"\"meta-heading first\"> <h3 class=\"closed\">" "")
-                   (elim #"\"meta-heading last\"> <h3 class=\"closed\">"  "")
-                   (elim #"\"breadcrumb\">BGCI > Garden</div> <h1>"       "")
-                   (elim #"<a href=\"#tabs-1\">Garden Information</a>; <div id=\"tabs-1\"> <p>" "")
-                   (elim #"\"edit-actions\">(.*)alt=\"Edit this page\" /></a>" "")
-                   (elim #"<a href=\"#tabs-1\">" "")
-                   (elim #"<a href=\"#tabs-3\">"  "")
-                   (elim #"<a href=\"/joinin/members/\">" ";")
-                   (elim #"</h1>"       "")
-                   (elim #"<h3>"        "")
-                   (elim #"<b>"         "")
-                   (elim #"</b>"        "")
-                   (elim #"</h3>"       "")
-                   (elim #"</div>"      "")
-                   (elim #"<div>"       "")
-                   (elim #"<br />"      ";")
-                   (elim #"</li> </ul>" ";")
-                   (elim #"</li> <li>"  ";")
-                   (elim #"<ul> <li>"   ";")
-                   (elim #"<div id=\"tabs\">" "")
-                   (elim #"<div id=\"tabs\">" "")
-                   (elim #"<p>" "")
-                   (elim #"</p>" ""))
-             [name _ _ & others] p5
-             m  (reduce merge (sorted-map) (map-indexed tomap others))
-             m  (merge m (tomap 7 info))
-             p6 (assoc m "0-ID" id, "1-Name" name, "2-Info" info)
-;             p7 (join \newline (for [[k v] p6] (format "%s: %s" (subs k 2) v)))]
-             p7 (join \newline (for [[k v] p6] (format "%s: %s" k v)))]
-                (spit (str "parsed/" id ".txt") p7)
-                p6)))
+      (spit (str basename ".txt")
+            (with-out-str
+              (println "ID = " basename)
+              (doseq [[k v] (mapcat (fn [f] (f parsed))
+                                    [extract-name-field extract-familiar-fields extract-meta-heading-fields])]
+                (println k "=" v)))))))
 
+(defn -main
+  [& args]
+  (when (zero? (count args))
+    (println "Usage: reaver html-files")
+    (System/exit 1))
 
-(def db (parse-all ids))
+  (work args))
 
-(map count db)
-
-(def allkeys (filter #(< (count %) 50) (flatten (map keys db))))
-(spit "good-keys.txt" (join \newline (sort (map first (take 100 (sort-by val > (frequencies allkeys)))))))
-
-; (def good-keys (sort (map first (take 100 (sort-by val > (frequencies allkeys))))))
-(def good-keys (split (slurp "good-keys.txt") #"\n"))
-
-(def header (map #(subs % 2) good-keys))
-(def rows   (map (fn [garden] (map #(get garden %) good-keys)) db))
-(def xls (into [header] rows))
-
-(export-xls xls "gardens.xlsx")
-
-;(frequencies allkeys)
-;(take 100 (sort-by val > (frequencies allkeys)))
-;
-;(take 100 (sort-by val > (frequencies allkeys)))
-;
-;(spit "good-keys.txt" (sort (map first (take 100 (sort-by val > (frequencies allkeys))))))
-;
-;
-;(set (flatten (map keys db)))
-;
-;                (println (count db)
-;                         (keys (first db)
-;                         (vals (first db))))
-
-(defn main [& args] 1)
-
-
-
-
-
-
+(export-xls "a.xlsx" [["A11" "B12"][10 20][30 40]])
