8078cff72d3d31025502e348c2a50d9267d32d6a
Tim Ashchepkov <tashepkov@gmail.com>
2024-06-20 15:14:32 -0400

    Produced Excel

--- src/botsad/core.clj
+++ src/botsad/core.clj
@@ -1,6 +1,7 @@
 (ns botsad.core
   (:require [reaver                       :as reaver]
             [clojure.string               :as str]
+            [clojure.pprint               :as pp]
             [dk.ative.docjure.spreadsheet :as xls]))
 
 (defn get-key-and-value
@@ -22,12 +23,17 @@
 
 (defn get-contact-fields
   [s]
+;;   (println "here")
+;;   (println "s=" s)
+;;   (println "there")
   (let [[_all _phone phone _fax fax _url url _email email]
         (re-find #"(Telephone:)(.*)(Fax:)(.*)(URL:)(.*)(Primary Email:)(.*)" s)]
-    [["Phone" (str/trim phone)]
-     ["Fax" (str/trim fax)]
-     ["URL" (str/trim url)]
-     ["Email" (str/trim email)]]))
+    (if (nil? phone)
+      []
+      [["Phone" (str/trim phone)]
+       ["Fax" (str/trim fax)]
+       ["URL" (str/trim url)]
+       ["Email" (str/trim email)]])))
 
 (defn get-familiar-field
   [s]
@@ -52,7 +58,7 @@
         _  (xls/select-sheet "Gardens" w)]
     (xls/save-workbook! file w)))
 
-(defn work
+#_(defn parse-to-files
   [urls]
   (doseq [url urls]
     (println "Processing" url)
@@ -60,12 +66,40 @@
           parsed (-> url slurp (str/replace #"</?[bi]>" "")
                      reaver/parse)]
 
-      (spit (str basename ".txt")
+      (spit (str "parsed/" basename ".txt")
             (with-out-str
               (println "ID = " basename)
               (doseq [[k v] (mapcat (fn [f] (f parsed))
                                     [extract-name-field extract-familiar-fields extract-meta-heading-fields])]
                 (println k "=" v)))))))
+
+(defn work
+  [urls]
+  (let [db
+        (reduce (fn [acc url]
+                  (println url)
+                  (let [[_all _slash garden-id _ext] (re-find #"(/|\\)?(\d+)(.html|htm)" url)
+                        parsed (-> url
+                                   slurp
+                                   (str/replace #"</?[bi]>" "")
+                                   reaver/parse)]
+                    (assoc acc
+                           garden-id
+                           (reduce (fn [garden [k v]]
+                                     (assoc garden k v))
+                                   (sorted-map)
+                                   (mapcat (fn [f]
+                                             (f parsed)) [extract-name-field
+                                                          extract-familiar-fields
+                                                          extract-meta-heading-fields])))))
+                (sorted-map)
+                urls)]
+
+    (spit
+     "parsed/DB.edn"
+     (with-out-str
+       (pp/pprint db)))))
+
 
 (defn -main
   [& args]
@@ -75,4 +109,60 @@
 
   (work args))
 
-(export-xls "a.xlsx" [["A11" "B12"][10 20][30 40]])
+(comment
+  (def edn (read-string (slurp "parsed/DB.edn")))
+
+  (count (keys edn))
+
+  (defn edn->xls [edn-file])
+
+  (def test {1 {:aa 1, :ab 2},
+             2 {:ba 2, :bb 3}})
+
+  (map (fn [[k v]] v) test)
+
+  (spit "parsed/KEYS"
+        (->> edn
+             (map (fn [[_k v]] (keys v)))
+             flatten
+             (reduce (fn [acc k] (update acc k #(inc (or % 1))))
+                     (sorted-map))
+             pp/pprint
+             with-out-str))
+
+  (spit "parsed/KEYSTOP100"
+        (->> edn
+             (map (fn [[_k v]] (keys v)))
+             flatten
+             (reduce (fn [acc k] (update acc k #(inc (or % 1))))
+                     (sorted-map))
+             (filter (fn [[_k v]] (> v 100)))
+             pp/pprint
+             with-out-str))
+
+  (def selected-keys
+    (->> edn
+         (map (fn [[_k v]] (keys v)))
+         flatten
+         (reduce (fn [acc k] (update acc k #(inc (or % 1))))
+                 (sorted-map))
+         (filter (fn [[_k v]] (> v 100)))
+         (map first)))
+
+  selected-keys
+
+  (def rows   (map (fn [[id garden]]
+                     (cons id
+                           (map #(get garden %) selected-keys)))
+                   edn))
+  (def selected-keys-with-id (cons "ID" selected-keys))
+
+  (def xls (cons selected-keys-with-id rows))
+
+
+
+  (second xls)
+
+  (export-xls "a.xlsx" xls)
+
+  )
